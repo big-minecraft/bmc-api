1e08c56a284d0a7d8c75972812054a21916a2cdd
wiji <wijigaming.yt@gmail.com>
2024-11-07 17:06:21 -0800

    add enum for redis channels

--- src/main/java/dev/wiji/bigminecraftapi/enums/RedisChannel.java
+++ src/main/java/dev/wiji/bigminecraftapi/enums/RedisChannel.java
@@ -4,7 +4,7 @@ public enum RedisChannel {
 
 	QUEUE_PLAYER("queue-player"),
 	QUEUE_RESPONSE("queue-response"),
-	INSTANCE_UPDATE("instance-updated"),
+	INSTANCE_MODIFIED("instance-modified"),
 	PROXY_CONNECT("proxy-connect"),
 	PROXY_DISCONNECT("proxy-disconnect"),
 	INSTANCE_SWITCH("instance-switch"),
@@ -12,6 +12,7 @@ public enum RedisChannel {
 	INITIAL_INSTANCE_RESPONSE("initial-instance-response"),
 	INSTANCE_STATE_CHANGE("instance-state-change"),
 	PROXY_REGISTER("proxy-register"),
+	GAMEMODE_MODIFIED("gamemode-modified"),
 	;
 
 	private final String ref;
