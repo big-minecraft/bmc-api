e20b214c46e6e3af4a99aff2567ae6f17df66ec1
wiji <wijigaming.yt@gmail.com>
2025-01-13 18:38:29 -0800

    remove protected access from addListener method

--- src/main/java/dev/wiji/bigminecraftapi/controllers/RedisManager.java
+++ src/main/java/dev/wiji/bigminecraftapi/controllers/RedisManager.java
@@ -12,7 +12,7 @@ public class RedisManager {
 		pool = new JedisPool("redis-service", 6379);
 	}
 
-	void addListener(RedisListener listener) {
+	public void addListener(RedisListener listener) {
 		JedisPubSub pubSub = new JedisPubSub() {
 			@Override
 			public void onMessage(String channel, String message) {
